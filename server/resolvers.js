1b56a45d90b8b3b047672cbc47b50b6a403462ab
khosrora <khosrora153333@gmail.com>
2024-05-22 03:29:17 -0400

    integrated graphql and express

---
+++ server/resolvers.js
@@ -0,0 +1,11 @@
+export const resolvers = {
+    // Query: {
+    //     Job: () => {
+    //         return {
+    //             id: 'test_id',
+    //             title: 'developer',
+    //             description: 'hello world'
+    //         }
+    //     }
+    // }
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import cors from 'cors';
 import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
+import { ApolloServer } from '@apollo/server';
+import { readFile } from 'fs/promises'
+import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
+import { resolvers } from './resolvers.js'
 
 const PORT = 9000;
 
@@ -9,6 +13,16 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
+const typeDefs = await readFile('./schema.graphql', 'utf-8');
+
+const apolloService = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+await apolloService.start();
+app.use('/graphql', apolloMiddleware(apolloService))
+
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL running on http://localhost:${PORT}/graphql`);
 });
